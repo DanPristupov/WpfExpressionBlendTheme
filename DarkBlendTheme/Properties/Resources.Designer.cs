103ee0632e913aff5931de4f1000a8e9292f3a42
Tanya <danil@danil.cz>
2014-07-01 08:50:45 +0200

    Adds image resource to the project.

--- DarkBlendTheme/Properties/Resources.Designer.cs
+++ DarkBlendTheme/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace DarkBlendTheme.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Icon {
+            get {
+                object obj = ResourceManager.GetObject("Icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
