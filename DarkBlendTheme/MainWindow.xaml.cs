88956361ae0ecd1a8a4046b1826a2b5b029ef6e0
Dan <zyrkzyrk@gmail.com>
2014-08-23 11:45:40 +0200

    Adds DataGrid to  MainWindow

--- DarkBlendTheme/MainWindow.xaml.cs
+++ DarkBlendTheme/MainWindow.xaml.cs
@@ -23,6 +23,31 @@ namespace DarkBlendTheme
         public MainWindow()
         {
             InitializeComponent();
+            var users = new List<User>
+            {
+                new User()
+                {
+                    Id = 1,
+                    Name = "John Doe",
+                    Birthday = new DateTime(1971, 7, 23),
+                    ImageUrl = "http://www.wpf-tutorial.com/images/misc/john_doe.jpg"
+                },
+                new User() {Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17)},
+                new User() {Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2)}
+            };
+
+            DataGrid.ItemsSource = users;
         }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
